4b44bb0f3d9a6367123be3a67fd6e681e59109e5
drixmix <foxyhorse@gmail.com>
2016-10-28 13:52:28 +0200

    Display specific post, in a separate page working

--- routes/index.js
+++ routes/index.js
@@ -4,25 +4,8 @@
 
   var path = ['index', 'createpost', 'post']
 
-  // var fanArt = [
-  //
-  //     {
-  //         name: 'Walther White',
-  //         episode: '12',
-  //         image: 'https://i.embed.ly/1/display/resize?key=1e6a1a1efdb011df84894040444cdc60&url=http%3A%2F%2F25.media.tumblr.com%2Ftumblr_lxluhkggrX1qaps3fo1_r1_1280.jpg&width=810'
-  //     }, {
-  //         name: 'Jesse',
-  //         episode: '43',
-  //         image: 'https://i.embed.ly/1/display/resize?key=1e6a1a1efdb011df84894040444cdc60&url=http%3A%2F%2F25.media.tumblr.com%2Ftumblr_lxlumt4VvJ1qaps3fo1_r1_1280.jpg&width=810'
-  //     }, {
-  //         name: 'Skyler White',
-  //         episode: '1',
-  //         image: 'https://i.embed.ly/1/display/resize?key=1e6a1a1efdb011df84894040444cdc60&url=http%3A%2F%2F25.media.tumblr.com%2Ftumblr_lxluj3nP0x1qaps3fo1_r1_1280.jpg&width=810'
-  //     }
-  //
-  // ]
 
-  /* GET home page. */
+// Find/get all posts in the databse
   router.get('/', function(req, res, next) {
 
       Post.find(null, function(err, posts) {
@@ -41,6 +24,7 @@
 
   });
 
+//Get specific page
   router.get('/:page', function(req, res, next) {
 
       var page = req.params.page
@@ -66,13 +50,46 @@
               return
           }
 
-          res.render(page, post)
+          data = {
+            name: post.name,
+            episode: post.episode,
+            image: post.image
+
+          }
+
+          res.render(page, data)
 
       })
 
 
 
   });
+
+  router.delete('/:page/:id', function(req, res, next){
+    var page = req.params.page
+    var id = req.query.id // Id number of Post we want to render
+
+    if (path.indexOf(page) == -1) {
+        res.render('error', {
+            message: 'Check your Spellin boooYYY!'
+        })
+        return
+      }
+
+    var did = req.params.id
+    var bdoy = req.body
+
+
+    Post.findByIdAndRemove(did, bdoy, function(err, post){
+      if (err) return next(err);
+      res.redirect('/')
+      console.log('Delete ')
+    })
+
+
+
+
+  })
 
   router.post('/:page', function(req, res, next) {
 
